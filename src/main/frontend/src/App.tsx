779f2dfea6995a29f030217b661b40b3f6b4feae
grzesiek-git <nowak.grzegorz140@gmail.com>
2022-12-16 19:11:57 +0100

    init

---
+++ src/main/frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
