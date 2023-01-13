779188e347e5722e5522f1698956ed526cf52706
grzesiek-git <nowak.grzegorz140@gmail.com>
2023-01-13 16:59:30 +0100

    Add beautifull dnd and ant design

--- src/main/java/com/proj/todoapp/repository/TodoRepo.java
+++ src/main/java/com/proj/todoapp/repository/TodoRepo.java
@@ -1,13 +1,8 @@
 package com.proj.todoapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.proj.todoapp.model.TodoItem;
-
-import jakarta.transaction.Transactional;
 
 public interface TodoRepo extends JpaRepository<TodoItem, Long> {
 
