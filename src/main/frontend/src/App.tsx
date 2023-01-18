6338d4b1ca83e1c1e0e15ec0873fa62a1b727732
Krzysztof Tomecki <113407220+christomecki@users.noreply.github.com>
2023-01-18 12:43:23 +0100

    #2 ESLint & Prettier

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -1,57 +1,48 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
 import { Button, Modal, Popconfirm, Col, Row, Typography } from 'antd';
 import { PlusOutlined, DeleteOutlined, CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 
 type Todo = {
-  id: number
-  title: string
-  done: boolean
-}
+  id: number;
+  title: string;
+  done: boolean;
+};
 
 function App() {
-
   const { Title } = Typography;
 
+  const [todos, setTodos] = useState<Todo[] | undefined>()
   const [todoItem, setTodoItem] = useState<string | undefined>();
   const onChange = (e: React.FormEvent<HTMLInputElement>) => setTodoItem(e.currentTarget.value);
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const update = () => {
-    if (isLoading)
-      return;
-    setIsLoading(true)
+    if (isLoading) return;
+    setIsLoading(true);
     fetch('api/get')
       .then((response) => response.json())
       .then((data) => {
-        setTodos(data as Todo[])
+        setTodos(data as Todo[]);
       })
-      .finally(() => setIsLoading(false))
-  }
+      .finally(() => setIsLoading(false));
+  };
 
   const save = () => {
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title: todoItem })
+      body: JSON.stringify({ title: todoItem }),
     };
     fetch('api/save', requestOptions)
-      .then(response => response.json())
-      .then(() => update())
+      .then((response) => response.json())
+      .then(() => update());
   };
 
   const remove = (val: number) => {
-    fetch('api/remove/' + val, { method: 'DELETE' })
-      .then(() => update())
+    fetch('api/remove/' + val, { method: 'DELETE' }).then(() => update());
   };
-
-  const [todos, setTodos] = useState<Todo[] | undefined>()
-
-  useEffect(() => {
-    update();
-  }, []
-  )
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -75,15 +66,13 @@ function App() {
     <div>
       <Title>ToDoApp</Title>
 
-      <Row >
+      <Row>
         <Col span={8}>
           <div className="rcorners2">
             <div className="rcorners1" style={{ padding: '10px' }}>
               <Row wrap={false}>
-                <Col flex="auto" >
-                  <Title level={3}>
-                    ToDo items
-                  </Title>
+                <Col flex="auto">
+                  <Title level={3}>ToDo items</Title>
                 </Col>
                 <Col flex="50px">
                   <h3>
@@ -98,21 +87,17 @@ function App() {
               </Modal>
             </div>
             <div style={{ padding: '10px' }}>
-              {todos?.map((todos) =>
-                <div>
+              {todos?.map((todo) => (
+                <div key={todo.id}>
                   <Row wrap={false}>
-                    <Col flex="auto">
-                      {todos.title}
-                    </Col>
-                    <Col flex="50px">
-                      {todos.done === true ? <CheckCircleOutlined /> : <CloseCircleOutlined />}
-                    </Col>
+                    <Col flex="auto">{todo.title}</Col>
+                    <Col flex="50px">{todo.done === true ? <CheckCircleOutlined /> : <CloseCircleOutlined />}</Col>
                     <Col flex="50px">
                       <Popconfirm
                         placement="bottomLeft"
                         title={text}
                         description={description}
-                        onConfirm={() => remove(todos.id)}
+                        onConfirm={() => remove(todo.id)}
                         okText="Yes"
                         cancelText="No"
                       >
@@ -121,11 +106,9 @@ function App() {
                         </Button>
                       </Popconfirm>
                     </Col>
-
                   </Row>
-
                 </div>
-              )}
+              ))}
             </div>
           </div>
         </Col>
@@ -134,45 +117,40 @@ function App() {
             <div className="rcorners1" style={{ padding: '10px' }}>
               <Title level={3}>In progress</Title>
             </div>
-            <div style={{ padding: '10px' }}>
-
-            </div>
+            <div style={{ padding: '10px' }}></div>
           </div>
-
         </Col>
         <Col span={8}>
           <div className="rcorners2">
             <div className="rcorners1" style={{ padding: '10px' }}>
               <Title level={3}>Done</Title>
             </div>
-            <div style={{ padding: '10px' }}>
-
-            </div>
+            <div style={{ padding: '10px' }}></div>
           </div>
         </Col>
       </Row>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 //<Space wrap>
 //          <Button type="primary" value="Save" onClick={save}>Save</Button>
 //          <Button type="primary" value="Remove" onClick={remove}>Delete</Button>
 //        </Space>
 
-  //const save = () => fetch('api/save')
-  //  .then((data) => {
-  //    console.log(data)
-  //  });
+//const save = () => fetch('api/save')
+//  .then((data) => {
+//    console.log(data)
+//  });
 
-  //React.useEffect(() => {
-  //  const requestOptions = {
-  //    method: 'POST',
-  //    headers: { 'Content-Type': 'application/json' },
-  //    body: JSON.stringify({ title: 'React Hooks POST Request Example' })
-  //  };
-  //  fetch('api/save', requestOptions)
-  //    .then(response => response.json())
-  //    //.then(data => setPostId(data.id));
-  //}, []);
+//React.useEffect(() => {
+//  const requestOptions = {
+//    method: 'POST',
+//    headers: { 'Content-Type': 'application/json' },
+//    body: JSON.stringify({ title: 'React Hooks POST Request Example' })
+//  };
+//  fetch('api/save', requestOptions)
+//    .then(response => response.json())
+//    //.then(data => setPostId(data.id));
+//}, []);
