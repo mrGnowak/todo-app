e89fc15de273540bf20c6014a39f86c3c0211a92
grzesiek-git <nowak.grzegorz140@gmail.com>
2023-02-18 15:22:00 +0100

    JUnit tests

--- src/test/java/com/proj/todoapp/ToDoAppApplicationTests.java
+++ src/test/java/com/proj/todoapp/ToDoAppApplicationTests.java
@@ -35,7 +35,7 @@ class ToDoAppApplicationTests {
 	@DirtiesContext
 	void testRepoAdd() {
 		var item1 = new TodoItem(1L, "A1", "A", 2L);
-		var item2 = new TodoItem(2L, "A2", "A", null);
+		var item2 = new TodoItem(2L, "A2", "A", -1l);
 
 		todoRepo.save(item1);
 		todoRepo.save(item2);
@@ -50,24 +50,24 @@ class ToDoAppApplicationTests {
 		todoRepo.save(new TodoItem(1L, "A1", "A", 2L));
 		todoRepo.save(new TodoItem(2L, "A2", "A", 3L));
 		todoRepo.save(new TodoItem(3L, "A3", "A", 4L));
-		todoRepo.save(new TodoItem(4L, "A4", "A", null));
-		todoRepo.flush();
+		todoRepo.save(new TodoItem(4L, "A4", "A", 4l));
+		todoRepo.save(new TodoItem(5L, "A5", "A", -1l));
+		// todoRepo.flush();
 
 		// initial condition
-		// 1 -> 2 -> 3 -> 4
-
-		linkedList.updateDroppable(2L, 3L, "A");
-		todoRepo.flush();
+		// 1 -> 2 -> 3 -> 4 -> 5
+		// linkedList.updateDroppable(2L, 4L, "A");
+		// todoRepo.flush();
 
 		// expected order
-		// 1 -> 3 -> 2 -> 4
+		// 1 -> 4 -> 2 -> 3 -> 5
 
 		// TODO this line produces assertion error
-		// assertNextId(1L, 3L);
-
-		assertNextId(2L, 4L);
-		assertNextId(3L, 2L);
-		assertNextId(4L, null);
+		assertNextId(1L, 2L);
+		assertNextId(2L, 3L);
+		assertNextId(3L, 4L);
+		assertNextId(4L, 5L);
+		assertNextId(5L, -1l);
 	}
 
 	void assertNextId(long itemId, Long nextId) {
