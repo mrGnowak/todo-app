23e8df27bc9ee9dd7237ebd47b15e26cb5355c68
Krzysztof Tomecki <113407220+christomecki@users.noreply.github.com>
2023-03-08 09:57:18 +0100

    security config with cookie example

--- src/main/java/com/proj/todoapp/ToDoAppApplication.java
+++ src/main/java/com/proj/todoapp/ToDoAppApplication.java
@@ -2,8 +2,9 @@ package com.proj.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ToDoAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/proj/todoapp/security/SecurityConfig.java
+++ src/main/java/com/proj/todoapp/security/SecurityConfig.java
@@ -5,21 +5,59 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-// @EnableWebSecurity
+@EnableWebSecurity
 public class SecurityConfig {
 
-    // @Bean
-    // SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception
-    // {
-    // return http
-    // .authorizeRequests(
-    //
-    // )
-    // .build();
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder.encode("userPass"))
+                .roles("USER")
+                .build();
 
-    // }
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("adminPass"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .disable()
+                .csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/todoapp/**").authenticated()
+                .requestMatchers("/.**").permitAll()
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public enum Role {
+        ROLE_USER,
+        ROLE_ADMIN
+    }
 
 }
